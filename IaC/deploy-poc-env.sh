b52680c539876960683061d3a0f286dbc8b8547b
Omar Aviles <oaviles@live.com>
2022-06-20 16:40:51 -0500

    Update deploy-poc-env.sh

--- IaC/deploy-poc-env.sh
+++ IaC/deploy-poc-env.sh
@@ -2,11 +2,6 @@ RESOURCE_GROUP="Containers_Zone"
 LOCATION="eastus2"
 CONTAINERAPPS_ENVIRONMENT="oa-poc-env"
 
-CONTAINER_IMAGE_NAME=$4
-REGISTRY_SERVER=$5
-REGISTRY_USERNAME=$6
-REGISTRY_PASSWORD=$7
-
 az login --service-principal -u $1 -p $2 --tenant $3
 
 az extension add --name containerapp --upgrade
@@ -14,5 +9,3 @@ az provider register --namespace Microsoft.App
 az provider register --namespace Microsoft.OperationalInsights
 
 az containerapp env create --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --location $LOCATION
-
-az containerapp create --name oa-api --resource-group $RESOURCE_GROUP --image $CONTAINER_IMAGE_NAME --environment $CONTAINERAPPS_ENVIRONMENT --registry-server $REGISTRY_SERVER --registry-username $REGISTRY_USERNAME --registry-password $REGISTRY_PASSWORD
